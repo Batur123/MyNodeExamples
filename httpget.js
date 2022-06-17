0d21cf9637e583c2703c1f2e8fffab84540658f4
Batuhan Özkoç <batur.ozkoc@gmail.com>
2022-06-18 00:38:18 +0300

    Update httpget.js

--- httpget.js
+++ httpget.js
@@ -1,3 +1,11 @@
+let GetReqHTTPURL = getURL();
+let GetReqUserHTTPURL = getURL();
+let URLHeader = { timeout: 10000,headers: { 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040614 Firefox/0.8' }  };
+
+const PromiseBasedAxiosURL1 = axios.get(GetReqHTTPURL,URLHeader);
+const PromiseBasedAxiosURL2 = axios.get(GetReqUserHTTPURL,URLHeader);
+const XMLConvertHeaders = {ignoreComment: true, alwaysChildren: true, compact: true, trim: true, normalize: true, alwaysArray: true};
+
 Promise.all([PromiseBasedAxiosURL1,PromiseBasedAxiosURL2]).then(response => {
         if(response[0].status !== 200) return ErrorHandling.Handle("#RV0Y002",'[0]!200');
         if(response[1].status !== 200) return ErrorHandling.Handle("#RV0Y003",'[1]!200');
