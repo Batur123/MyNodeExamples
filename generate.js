dab327ae438e2f55ebaeaa7dfa762c70d71d6c90
Batuhan Özkoç <batur.ozkoc@gmail.com>
2022-06-18 00:38:44 +0300

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,32 +1,26 @@
-function generate(max, thecount)
-{
+const generate = (max, thecount) => {
     let r = [];
     let decimals = [];
     let currsum = 0;
-    for(let i=0; i<thecount; i++)
-    {
+    for (let i = 0; i < thecount; i++) {
         r.push(Math.random());
         currsum += r[i];
     }
 
     let remaining = max;
-    for(let i=0; i<r.length; i++) 
-    {
+    for (let i = 0; i < r.length; i++) {
         let res = r[i] / currsum * max;
         r[i] = Math.floor(res);
         remaining -= r[i];
         decimals.push(res - r[i]);
     }
 
-    while(remaining > 0)
-    {
+    while (remaining > 0) {
         let maxPos = 0;
         let maxVal = 0;
 
-        for(let i=0; i<decimals.length; i++)
-        {
-            if(maxVal < decimals[i])
-            {
+        for (let i = 0; i < decimals.length; i++) {
+            if (maxVal < decimals[i]) {
                 maxVal = decimals[i];
                 maxPos = i;
             }
@@ -38,7 +32,7 @@ function generate(max, thecount)
     }
 
     return r;
-}
+};
 
 let arr = generate(100,4)
 console.log(arr);
