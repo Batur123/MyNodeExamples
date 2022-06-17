438fafaed6db887bfc02b687f72276738f44284a
Batuhan Özkoç <batur.ozkoc@gmail.com>
2022-06-18 00:42:19 +0300

    Update flood_protection.js

--- flood_protection.js
+++ flood_protection.js
@@ -1,55 +1,43 @@
-
 const LIMIT = 0
 const DIFF = 0
 const TIME =0
+let usersMap = new Map();
 
 const floodProtection = (username,message,channelName) => {
-{
     let newDate = new Date();
-    if(usersMap.has(username))
-    {
+    if (usersMap.has(username)) {
         const userData = usersMap.get(username);
-        const { lastMessage, timer } = userData;
+        const {lastMessage, timer} = userData;
         const difference = newDate.getTime() - lastMessage;
         let msgCount = userData.msgCount;
 
-        if(difference > DIFF)
-        {
+        if (difference > DIFF) {
             clearTimeout(timer);
             userData.msgCount = 1;
             userData.lastMessage = message;
-            userData.timer = setTimeout(() =>
-            {
+            userData.timer = setTimeout(() => {
                 usersMap.delete(nick);
             }, TIME);
             usersMap.set(username, userData)
-        }
-        else
-        {
+        } else {
             ++msgCount;
-            if(parseInt(msgCount) === LIMIT)
-            {
-               console.log("Spam detected");
-            }
-            else
-            {
+            if (parseInt(msgCount) === LIMIT) {
+                console.log("Spam detected");
+            } else {
                 userData.msgCount = msgCount;
                 usersMap.set(username, userData);
             }
         }
-    }
-    else
-    {
-        let fn = setTimeout(() =>
-        {
+    } else {
+        let fn = setTimeout(() => {
             usersMap.delete(username);
         }, TIME);
 
         usersMap.set(username,
-        {
-            msgCount: 1,
-            lastMessage : newDate.getTime(),
-            timer : fn,
-        });
+            {
+                msgCount: 1,
+                lastMessage: newDate.getTime(),
+                timer: fn,
+            });
     }
-});
+}
